3da9c25b3799b6cc7645e036a9d7ae303dd54d20
Gareth Slinn <gslinn@salmon.com>
2015-05-10 21:29:21 +0100

    second commit

---
+++ Gulpfile.js
@@ -0,0 +1,65 @@
+/**
+ * Created by Gareth Slinn April 2015
+ */
+
+var gulp = require('gulp');
+var sass = require( 'gulp-ruby-sass' );
+var path = require( 'path' );
+var compass = require( 'gulp-compass' );
+var concat = require( 'gulp-concat' );
+var uglify = require( 'gulp-uglify' );
+var rename = require( 'gulp-rename' );
+var jshint = require('gulp-jshint');
+
+var config = {
+    scripts: {
+        jsVendorSrc: [ 'src/public/js/ext/jquery-1.11.1.min.js' ],
+        jsSrc: [ 'src/public/js/base.js' ],
+        outputName: 'all.js',
+        outputVendorName: 'vendor.js',
+        dest: 'dist/js/'
+    },
+    styles: {
+        src: [ 'src/public/scss/base.scss' ]
+    }
+};
+gulp.task('default', function() {
+    // No current default tasks
+});
+
+gulp.task('compass', function(){
+    return gulp
+        .src( config.styles.src )
+        .pipe( compass({
+            project: path.join( __dirname, 'src/public' ),
+            sass: 'scss',
+            image: 'img',
+            css: '../../dist/css/'
+        }));
+});
+
+gulp.task('scripts:vendor', function() {
+    return gulp
+        .src( config.scripts.jsVendorSrc )
+        .pipe( concat( config.scripts.outputVendorName ) )
+        .pipe( gulp.dest( config.scripts.dest ) )
+        .pipe( uglify() )
+        .pipe( rename( { extname: '.min.js' } ) )
+        .pipe( gulp.dest( config.scripts.dest ) );
+});
+
+gulp.task('scripts:localStorageFlickr', function() {
+    return gulp
+        .src( config.scripts.jsSrc )
+        .pipe( concat( config.scripts.outputName ) )
+        .pipe( gulp.dest( config.scripts.dest ) )
+        .pipe( uglify() )
+        .pipe( rename( { extname: '.min.js' } ) )
+        .pipe( gulp.dest( config.scripts.dest ) );
+});
+
+gulp.task('lint', function() {
+    return gulp.src('src/public/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
---
+++ README.md
@@ -0,0 +1,60 @@
+# Node app that uses a Flickr feed and stores active status in local storage
+
+# Dependencies
+* Node
+* Npm
+* Ruby
+
+Ensure you have Gulp installed:
+
+```bash
+npm install -g gulp
+```
+
+# Setup the project
+
+Install the Node dependencies from npm:
+
+```bash
+npm install
+```
+
+Run the Gulp build:
+```bash
+gulp
+```
+
+# Gulp
+
+To build the static files:
+
+```bash
+gulp
+```
+
+To lint:
+
+```bash
+gulp lint
+```
+
+To build just the JavaScripts:
+
+```bash
+gulp scripts:localStorageFlickr
+```
+
+To build just the Vendor JavaScripts:
+
+```bash
+gulp scripts:vendor
+```
+
+To build just the css:
+
+```bash
+gulp compass
+```
+
+
+# localStorageFlickr 
